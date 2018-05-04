284cc6a2a0348d019e4691a5753a3a8c7bb1d48c
Marcus <2161524@slu.edu.ph>
2018-05-04 19:03:25 +0800

    Modification to express

--- Node_Module/README.md
+++ Node_Module/README.md
@@ -1,30 +1,29 @@
 # Node useful links
 * [Basics](https://www.youtube.com/watch?v=U8XF6AFGqlc)
-* [Node js routing](https://www.youtube.com/watch?v=tiMLxUKrB-g)
 * [MySQL](https://www.youtube.com/watch?v=EN6Dx22cPRI)
 * [Node MySQL query](https://www.w3schools.com/nodejs/nodejs_mysql_select.asp)
-* [Serve multiple files](https://stackoverflow.com/questions/49967578/how-do-i-serve-multiple-files-without-express)
-* [Node MySQL valuable information](https://www.youtube.com/watch?v=XuLRKMqozwA)
-* [Node handling post request](https://itnext.io/how-to-handle-the-post-request-body-in-node-js-without-using-a-framework-cd2038b93190)
 * [Node js get form input](https://www.w3schools.com/nodejs/ref_querystring.asp)
+* [Templating Engine EJS](https://www.youtube.com/watch?v=RgAseumFyg8)
+* [Serve static files (CSS)](https://expressjs.com/en/starter/static-files.html)
 
-## Required modules
+## Modules
+* `npm install express` - the root of everything
+* `npm install body-parser` - for handling post request
+* `npm install ejs` - for templating engine
+* `npm install mysql` - database access
+* `npm install sha256` - password encryption
 * `npm install --save-dev nodemon` or `npm install -g nodemon` (optional - automatic server restart)
-* `npm install mysql`
-* `npm install sha256`
+* `npm install express body-parser ejs mysql sha256` - one line installation
 
-### How to change domain name of server
+## How to change domain name of server
 
-Step 1: Open WAMP server. Then left-click on the Wamp Icon
-Step 2: Under Apache, select httpd.conf
-Step 3: Change the port number to 3000 (or any number except 80) of the following lines to desired port:
-
-Listen 0.0.0.0:3000
-Listen [::0]:3000
-
-ServerName localhost:3000
-
-Step 4: Change the hosts file to 127.0.0.1 transientvania.com
-Step 5: Inside the Node app change the port number of the server to port 80
-Step 6: Restart Wamp Server.
-Step 7: Run Node app and test on the browser.
+1. Open WAMP server. Then left-click on the Wamp Icon
+2. Under Apache, select `httpd.conf`
+3. Change the port number to `3000` (or any number except 80) of the following lines to desired port:
+   * `Listen 0.0.0.0:3000`
+   * `Listen [::0]:3000`
+   * `ServerName localhost:3000`
+4. Change the hosts file to `127.0.0.1 transientvania.com`
+5. Inside the Node app, change the port number of the server to port `80`
+6. Restart Wamp Server.
+7. Run Node app and test on the browser.
--- Node_Module/app.js
+++ Node_Module/app.js
@@ -1,107 +1,58 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const sha256 = require('sha256');
-const qs = require('querystring');
-const tc = require('./text-color');
+
+const app = express();
 
 const host = 'localhost';
 const port = 3000;
 const db = 'webtekfinals';
 const sql = 'SELECT * FROM webtekfinalstable';
-let tableRow;
 
-// Create database connection variable
+let dbData;
+
 const con = mysql.createConnection({
   host: host,
   user: 'root',
   password: '',
   database: db
 });
-
-// Connect to database
-con.connect(err => {
+con.connect();
+con.query(sql, (err, rows, fields) => {
   if (err) {
     throw err;
   }
-  console.log(tc.success(`Connected to ${db} database!`));
+  dbData = rows;
+});
+// con.end();
+
+app.set('view engine', 'ejs');
+
+app.use('/styles', express.static(__dirname + '/views/styles'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-// Query to the database
-con.query(sql, (err, result) => {
-  if (err) {
-    throw err;
-  }
-  tableRow = result;
-});
-
-// Server Connection
-http.createServer((req, res) => {
-  console.log(req.method, req.url);
-  if (req.method === 'GET') {
-    switch (req.url) {
-      case '/':
-        renderHTML(res, './public/index.html', 'text/html');
-        break;
-      case '/styles/style.css':
-        renderHTML(res, './public/styles/style.css', 'text/css');
-        break;
-      default:
-        res.writeHead(404, {
-          'Content-type': 'text/plain'
-        });
-        res.end('404 Not found!');
-        break;
-    }
-  } else if (req.method === 'POST') {
-    req.on('data', chunk => {
-      let username = qs.parse(chunk.toString()).username;
-      let password = sha256(qs.parse(chunk.toString()).password);
-      tableRow.forEach(row => {
-        if (username.toLowerCase() == row.username.toLowerCase()) { // To avoid case sensitivity
-          if (password === row.password) {
-            console.log(tc.info(row.username), tc.success('logged in!'));
-            let csjs = `<script>$(function() {$('.user').append('${row.username}');});</script>`;
-            renderHTML(res, './public/dashboard.html', 'text/html', csjs);
-          } else {
-            let csjs = "<script>$(function() {$('small').append('Wrong password');});</script>";
-            renderHTML(res, './public/index.html', 'text/html', csjs);
-          }
-        } else {
-          let csjs = "<script>$(function() {$('small').append('Username not found');});</script>";
-          renderHTML(res, './public/index.html', 'text/html', csjs);
-        }
-      });
-    });
-  } else {
-    fs.readFile(path, (err, content) => {
-      res.writeHead(200, {
-        'Content-type': 'text/html'
-      });
-      res.end('Request error');
-    });
-  }
-}).listen(port, host, () => { // Listen to port
-  console.log(tc.info(`\nListening to ${host} on port ${port}`));
-});
-
-// Render html code
-let renderHTML = (res, path, mime, csjs) => {
-  fs.readFile(path, (err, content) => {
-    res.writeHead(200, {
-      'Content-type': mime
-    });
-    if (csjs == null) {
-      res.write(content);
+app.post('/dashboard', (req, res) => {
+  let username = req.body.username;
+  let password = sha256(req.body.password);
+  dbData.forEach(row => {
+    if (row.username.toLowerCase() == username.toLowerCase()) {
+      if (row.password === password) {
+        res.render('dashboard', {user: row.username});
+      } else {
+        res.end('wrong password');
+      }
     } else {
-      res.write(`${content}\n${csjs}`);
+      res.end('wrong username');
     }
-    if (mime !== 'text/css') {
-      res.write(`
-        </body>
-        </html>
-      `);
-    }
-    res.end();
   });
-}
+});
+
+app.listen(port, host, () => {
+  console.log(`Connected to ${host} on port ${port}!`);
+});
