d54140de9b773f1403b554e97eacbc439179c2c3
Rizalde <velascorizalde@gmail.com>
2018-05-11 12:47:30 +0800

    Fixed cant set header on wrong password

--- Node_Module/app.js
+++ Node_Module/app.js
@@ -9,9 +9,9 @@ const app = express();
 
 const host = 'localhost';
 const port = 3000;
-const db = 'webtekfinals';
-const sql = 'SELECT * FROM webtekfinalstable';
+const db = 'transient';
 
+let sql = 'SELECT * FROM provider';
 let dbData;
 let username;
 
@@ -22,10 +22,10 @@ const con = mysql.createConnection({
   database: db
 });
 con.connect(err => {
-	if (err) {
+  if (err) {
     console.log(tc.text('error', 'database connection error!'));
-		throw err;
-	} else {
+    throw err;
+  } else {
     console.log(tc.text('suc', `Connected to ${db} database!`));
   }
 });
@@ -42,41 +42,59 @@ con.end();
 app.set('view engine', 'ejs');
 
 app.use('/static', express.static(__dirname + '/views/static'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
-app.use(session({secret: 'webtek'}));
+app.use(session({
+  secret: 'webtek'
+}));
 
 app.get('/', (req, res) => {
   if (req.session.username) {
-    res.redirect('/dashboard', {user: username}, 302);
+    res.redirect('/dashboard', {
+      user: username
+    }, 302);
   } else {
-    res.render('index', {callback: ''});
+    res.render('index', {
+      callback: ''
+    });
   }
 });
 
 app.get('/dashboard', (req, res) => {
   if (req.session.username) {
-    res.render('dashboard', {user: username});
+    res.render('dashboard', {
+      user: username
+    });
   } else {
-    res.render('index', {callback: 'You are not logged in!'});
+    res.render('index', {
+      callback: 'You are not logged in!'
+    });
   }
 });
 
 app.post('/dashboard', (req, res) => {
   username = req.body.username;
-  let password = sha256(req.body.password);
+  let password = req.body.password;
   dbData.forEach(row => {
-    if (row.username.toLowerCase() == username.toLowerCase()) {
-      if (row.password === password) {
+    if (row.prov_username.toLowerCase() === username.toLowerCase()) {
+      if (row.prov_pswd === password) {
         req.session.username = username;
         req.session.username = password;
-        res.render('dashboard', {user: row.username});
-        console.log(tc.text('info', `${row.username} logged in!`));
-      } else {
-        res.render('index', {callback: 'Wrong password!'});
+        res.render('dashboard', {
+          user: row.prov_username
+        });
+        console.log(tc.text('info', `${row.prov_username} logged in!`));
+      } else if (row.prov_pswd !== password) {
+        res.render('index', {
+          callback: 'Wrong password!'
+        });
       }
-    } else {
-      res.render('index', {callback: 'Username not found!'});
+    } else if (row.prov_username.toLowerCase() !== username.toLowerCase() && row.prov_pswd !== password) {
+      res.render('index', {
+        callback: 'Username not found!'
+      });
     }
   });
 });
@@ -86,25 +104,35 @@ app.get('/logout', (req, res) => {
     if (err) {
       res.negotiate(err);
     } else {
-      res.redirect('/', {callback: 'test'}, 302);
+      res.redirect('/', {
+        callback: 'test'
+      }, 302);
       console.log(tc.text('warning', `${username} logged out!`));
     }
   });
 });
 
-app.get('/listings', (req,res) => {
+app.get('/listings', (req, res) => {
   if (req.session.username) {
-    res.render('listings', {user: username});
+    res.render('listings', {
+      user: username
+    });
   } else {
-    res.render('index', {callback: 'You are not logged in!'});
+    res.render('index', {
+      callback: 'You are not logged in!'
+    });
   }
 });
 
 app.get('/new-entry', (req, res) => {
   if (req.session.username) {
-    res.render('newEntry', {user: username});
+    res.render('newEntry', {
+      user: username
+    });
   } else {
-    res.render('index', {callback: 'You are not logged in!'});
+    res.render('index', {
+      callback: 'You are not logged in!'
+    });
   }
 });
 
